2f64bab9fb3996917d9f6ffebcf2422674dd27e8
GurLad <40135047+GurLad@users.noreply.github.com>
2021-08-24 10:43:17 +0300

    Added Ali's changes

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request, url_for
+from backend import Retrieve_PIO as rPIO
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+	return render_template('home.html')
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    population = request.form.get('population')
-    intervention = request.form.get('intervention')
-    return render_template('search.html')
+	if request.method == 'POST':
+		population = request.form.get('population')
+		others = request.form.get('others')
+		output = rPIO.get_PIO(population,others)
+		return render_template('search.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
---
+++ backend/Retrieve_PIO.py
@@ -0,0 +1,31 @@
+import json
+import Retrieval as ret
+import extract_PIO_elements as xPIO
+
+def create_ret_input(population, others):
+    pop_tag = "P"
+    Q = pop_tag + " " + population + " " + pop_tag + " " + others
+    #print(Q)
+    with open('test_abstracts.json', 'r') as json_file:
+        database_abstracts = json.load(json_file)
+    size = len(database_abstracts)
+    retrieval_input = [0 for element in range(size)]
+    for i in range(size):
+        retrieval_input[i] = [Q, database_abstracts[i]]
+    return retrieval_input
+
+def PIO(selected_abstracts):
+    size = len(selected_abstracts)
+    output_PIO = [0 for element in range(size)]
+    for i in range(size):
+        P_list, I_list, O_list = xPIO.extract_PIO(selected_abstracts[i])
+        output_PIO[i] = [P_list, I_list, O_list]
+        #print(output_PIO[i])
+    return output_PIO
+
+def get_PIO(population, others):
+    retrieval_input = create_ret_input(population, others)
+    selected_abstracts = ret.retrieval(retrieval_input)
+    output_PIO = PIO(selected_abstracts)
+    return output_PIO
+
