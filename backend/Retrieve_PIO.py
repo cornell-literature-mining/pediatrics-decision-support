14277836ff53cd14a3e5fc9ccaa625e8d517899d
GurLad <40135047+GurLad@users.noreply.github.com>
2021-09-02 13:42:08 +0300

    Working on UI

--- backend/Retrieve_PIO.py
+++ backend/Retrieve_PIO.py
@@ -1,12 +1,13 @@
 import json
-import Retrieval as ret
-import extract_PIO_elements as xPIO
+from . import Retrieval as ret
+from . import extract_PIO_elements as xPIO
+from . import File_loading as fl
 
 def create_ret_input(population, others):
     pop_tag = "P"
     Q = pop_tag + " " + population + " " + pop_tag + " " + others
     #print(Q)
-    with open('test_abstracts.json', 'r') as json_file:
+    with fl.Open('test_abstracts.json', 'r') as json_file:
         database_abstracts = json.load(json_file)
     size = len(database_abstracts)
     retrieval_input = [0 for element in range(size)]
@@ -24,10 +25,10 @@ def PIO(selected_abstracts):
     return output_PIO
 
 def find_entry(selected_abstract):
-    with open('database_info.json', 'r') as json_file:
+    with fl.Open('database_info.json', 'r') as json_file:
         database_info = json.load(json_file)
 
-    with open('PIO_data_PMID_abstracts.json', 'r') as json_file:
+    with fl.Open('PIO_data_PMID_abstracts.json', 'r') as json_file:
         database_abstracts = json.load(json_file)
 
     size_database = len(database_abstracts)
