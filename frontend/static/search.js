9516da970a7c212638bcc8b799d4f9803a8fb619
GurLad <40135047+GurLad@users.noreply.github.com>
2021-09-09 16:56:23 +0300

    Fixed tabs

--- frontend/static/search.js
+++ frontend/static/search.js
@@ -1,12 +1,3 @@
-import {
-    MDCRipple
-} from '@material/ripple';
-
-const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
-const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
-    return new MDCRipple(el);
-});
-
 var tabBar = new mdc.tabBar.MDCTabBar(document.querySelector('.mdc-tab-bar'));
 var contentEls = document.querySelectorAll('.content');
 
